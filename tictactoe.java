0a52a8ae6b212f00c5a78eb14decec6d40448bf6
unknown <naikmahesh.naik@gmail.com>
2021-02-27 09:03:07 +0530

    refactor code print board

--- tictactoe.java
+++ tictactoe.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class tictactoe
 {
+	static Scanner sc = new Scanner(System.in);
 
-public static void main(String[] args) 
+
+	public static void main(String[] args) 	
 	{
 		Scanner sc = new Scanner(System.in);
-
-		char[] board = boardDesign();
-		System.out.println(board);
-		System.out.println("player chose X or O");
+		System.out.println("player chose X or O");	
+		//System.out.println("player chose X or O\r");
 		char player = sc.next().charAt(0);
 
 		char computer = chooseLetter(player);
 		System.out.println("computer"+computer);
+		System.out.println("computer\r"+computer);
+
+		System.out.println("Let's start the game");
+		System.out.println("Current board is :");
+		showBoard();
+
 
 	}
 
@@ -23,7 +29,11 @@ public static void main(String[] args)
 		board[0] = ' ';
 
 		for(int i=1; i<10; i++)
-		{
+		//char[] board = new char[10];     
+
+		//for(int i=1; i<9; i++)
+
+	{
 			board[i] = ' ';
 		}
 
@@ -43,9 +53,28 @@ public static void main(String[] args)
 			char computer = 'X';
 			return computer;
 		}
+}
+
+				public static void showBoard()
+	{
+		char[] board = boardDesign();
+
+
+		System.out.println("         |                    |           ");
+		System.out.println(board[7]+"        |          "+board[8]+"         |          "+board[9]);
+  		System.out.println("---------|--------------------|-----------");
+	    System.out.println("         |                    |           ");
+	    System.out.println(board[4]+"        |          "+board[5]+"         |          "+board[6]);
+	    System.out.println("---------|--------------------|-----------");
+	    System.out.println("         |                    |           ");
+	    System.out.println(board[1]+"        |          "+board[2]+"         |          "+board[3]);
+
+	}
+
+
 
   }
 
 
-}
+
 
