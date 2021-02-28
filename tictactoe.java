5301a793123005e950c71d8c86951407cee0198b
unknown <naikmahesh.naik@gmail.com>
2021-02-28 23:02:15 +0530

    check for free space before making desired

--- tictactoe.java
+++ tictactoe.java
@@ -2,59 +2,61 @@ import java.util.Scanner;
 
 public class tictactoe {
 
-	static  char[] board = new char[10];
-	private static char ex = 'X';
-	private static char zero = '0';
+	static char[] board = new char[10];
+	private static char cross = 'X';
+	private static char zero = 'O';
 	private static char player;
 	private static char computer;
-	 
-	public static char[]  creatingBoard() {
 
+	// creating board char array and intialising it//
+	public static char[] creatingBoard() {
 
 		for (int i = 1; i < board.length; i++) {
-		board[i] = ' ';
+			board[i] = ' ';
 		}
 		return board;
-		}
-	public static void allowplayerchoose()
-	{
-		System.out.println("which symbol you want to choose X or 0");
+
+	}
+
+	// allowing player to choose symbol method
+	private static void allowPlayerToChoose() {
+		System.out.println("Enter the symbol X or O you want to choose");
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
 		player = input.charAt(0);
-		
-		if(player == ex)
-		{
-			player = ex;
+
+		if (player == cross) {
+			player = cross;
 			computer = zero;
 			System.out.println("player symbol is : " + player);
 
-		}
-		else if(player == zero)
-		{
+		} else if (player == zero) {
 			player = zero;
-			computer =ex;
+			computer = cross;
 			System.out.println("player symbol is : " + player);
-        }
-		else
-		{
-			System.out.println("Invalid");
+
+		} else {
+			System.out.println("Invalid input");
 		}
+
 	}
-	public static void showBoard()
-	{
-		System.out.println("Empty board");
-		System.out.println(" ");
-		System.out.println(" " +board[1]+ " " + "|" +" " +board[2]+ " " + "|" +"" +board[3]+ " ");
-		System.out.println("............");
-		System.out.println(" " +board[4]+ " " + "|" +" " +board[5]+ " " + "|" +"" +board[6]+ " ");
-		System.out.println("............");
-		System.out.println(" " +board[7]+ " " + "|" +" " +board[8]+ " " + "|" +"" +board[9]+ " ");
-		
+
+	// displaying board and values at specific positions
+	public static void showBoard() {
+		System.out.println("Empty board looks like :");
+		System.out.println("");
+		System.out.println("   " + board[1] + "   " + "|" + "   " + board[2] + "   " + "|" + "   " + board[3] + "   ");
+		System.out.println("  ---------------------");
+		System.out.println("   " + board[4] + "   " + "|" + "   " + board[5] + "   " + "|" + "   " + board[6] + "   ");
+		System.out.println("  ---------------------");
+		System.out.println("   " + board[7] + "   " + "|" + "   " + board[8] + "   " + "|" + "   " + board[9] + "   ");
 	}
+	
+	// taking position from player where he wants to put his input
+	//UC5 is also satisfied in this
 	public static void userMove()
 	{
-		System.out.println("Enter the position you want to move");
+		System.out.println("Enter the empty position(between 1-9) where you wants to make the move ");
 		Scanner sc = new Scanner(System.in);
 		int position = sc.nextInt();
 		if(position>=1 && position <=9)
@@ -77,16 +79,14 @@ public class tictactoe {
 	
 
 	}
-		
-	
-		
-		public static void main(String args[]) {
 
-			System.out.println("Welcome to tic tac board");
-			board = creatingBoard();
-			
-			allowplayerchoose();
-			showBoard();
-			userMove();
-		}
+	public static void main(String args[]) {
+
+		System.out.println("Welcome to tic tac board");
+		board = creatingBoard();
+		allowPlayerToChoose();
+		showBoard();
+		userMove();
+
 	}
+}
